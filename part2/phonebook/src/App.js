d7232601d65071c17f1e62f46bf1bb0c44f429ae
axxander <axander.gregory@gmail.com>
2021-10-13 08:07:22 +0100

    part2-d phonebook step7+8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Filter from './components/Filter';
 import Contacts from './components/Contacts';
 import NewContact from './components/NewContact';
+import personService from './services/person';
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
+
+    // get notes from server
+    useEffect(() => {
+        personService
+            .getAll()
+            .then(initialPersons => {
+                setPersons(initialPersons);
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }, []);
 
     const handleNameChange = (event) => {
         setNewName(event.target.value);
@@ -27,14 +35,24 @@ const App = () => {
             return;
         }
 
+        // create new person
         const personObject = {
             name: newName,
             number: newNumber,
             id: persons.length + 1,
         };
-        setPersons(persons.concat(personObject));
-        setNewName('');
-        setNewNumber('');
+
+        // post new person to backend
+        personService
+            .create(personObject)
+            .then(returnedPerson => {
+                setPersons(persons.concat(returnedPerson));
+                setNewName('');
+                setNewNumber('');
+            })
+            .catch(err => {
+                console.log(err);
+            });
     };
 
     const handleNumberChange = (event) => {
