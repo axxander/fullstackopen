2f9e46794506e934d7923b2bfb8e9ac490f6bb52
axxander <axander.gregory@gmail.com>
2021-10-23 20:56:42 +0100

    part2-d notes

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -8,8 +8,9 @@ const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState('a new note...');
   const [showAll, setShowAll] = useState(true);
-  const [errorMessage, setErrorMessage] = useState('some error happened...');
+  const [errorMessage, setErrorMessage] = useState(null);
 
+  // Initialise notes from server
   useEffect(() => {
     console.log('effect');
     noteService
@@ -18,44 +19,67 @@ const App = () => {
         setNotes(initialNotes);
       });
   }, []);
-  console.log('render', notes.length, 'notes');
 
+  // Adding a new note
   const addNote = (event) => {
     event.preventDefault(); // stop auto page reload
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote('');
+    noteService
+      .create(noteObject)
+      .then(note => {
+        setNotes(notes.concat(note));
+        setNewNote('');
+      })
+      .catch(err => {
+        console.log(err);
+        setErrorMessage(
+          `Sorry, something went wrong!`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+      });
   };
 
+  // Update new note state
   const handleNoteChange = (event) => {
-    console.log(newNote);
     setNewNote(event.target.value);
   };
 
+  // 
   const toggleImportanceOf = (id) => {
+    // Get note by id
     const note = notes.find(n => n.id === id);
+    // Toggle importance via negation
     const changedNote = { ...note, important: !note.important };
 
     noteService
       .update(id, changedNote)
       .then(returnedNote => {
-        setNotes(notes.map(note => note.id !== id ? note : returnedNote));
+        // update state depends on previous: so use callback function
+        setNotes(currentNotes => {
+          // only update note with given id
+          return currentNotes.map(n => n.id === id ? returnedNote : n);
+        });
       })
       .catch(err => {
+        // display error message
         setErrorMessage(
           `Note '${note.content}' was already removed from server`
         );
+        // remove error message after 5 seconds
         setTimeout(() => {
           setErrorMessage(null);
         }, 5000);
-        setNotes(notes.filter(n => n.id !== id));
-        setNotes(notes.filter(n => n.id !== id));
+        // delete note from DOM since already deleted on server
+        setNotes(currentNotes => {
+          return currentNotes.filter(note => note.id !== id);
+        });
       });
   };
 
