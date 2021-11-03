007eef94542705244620c65dc08e5af12c96e65e
axxander <axander.gregory@gmail.com>
2021-11-03 08:20:41 +0000

    Created error handling middleware

---
+++ part3/phonebook-backend/middleware/errors.middleware.js
@@ -0,0 +1,12 @@
+const errorHandler = (err, req, res, next) => {
+    const status = err.status || 500;
+    const msg = err.msg || "something went wrong";
+    return res.status(status).json({
+        error: {
+            msg,
+            status
+        }
+    });
+};
+
+module.exports = errorHandler;
