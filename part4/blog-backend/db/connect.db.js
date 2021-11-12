9467671402b9016e13fe6b1bbd69ce9636e4961f
axxander <axander.gregory@gmail.com>
2021-11-12 13:25:30 +0000

    Reorganised connection logic

--- part4/blog-backend/db/connect.db.js
+++ part4/blog-backend/db/connect.db.js
@@ -2,10 +2,16 @@ const mongoose = require('mongoose')
 
 const logger = require('../utils/logger.utils')
 
+
+const MONGO_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGO_URI
+    : process.env.MONGO_URI
+
+
 const connect = () => {
     logger.info('connecting to MongoDB')
     return mongoose.
-        connect(process.env.MONGO_URI, {
+        connect(MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         })
