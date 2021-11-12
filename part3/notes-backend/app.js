d8da30cfd0c50993601b7fbf6a1051db3d4fd2ca
axxander <axander.gregory@gmail.com>
2021-11-12 13:26:48 +0000

    Moved connection logic

--- part3/notes-backend/app.js
+++ part3/notes-backend/app.js
@@ -1,4 +1,3 @@
-const connect = require('./db/connect.db')
 const logger = require('./utils/logger.utils')
 const server = require('./utils/server.utils')
 
@@ -7,7 +6,4 @@ const app = server()
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     logger.info(`Server live at http://localhost:${PORT}`)
-
-    // connect to db
-    connect()
 })
