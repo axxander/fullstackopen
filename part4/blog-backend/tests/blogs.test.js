c678b91e51ba69d70e0fd8f597041da23702d2e1
axxander <axander.gregory@gmail.com>
2021-11-13 22:02:37 +0000

    Added additional tests

--- part4/blog-backend/tests/blogs.test.js
+++ part4/blog-backend/tests/blogs.test.js
@@ -11,10 +11,7 @@ const api = supertest(server())
 
 beforeEach(async () => {
     await Blog.deleteMany({})
-
-    const blogObjects = initialBlogs.map(blog => new Blog(blog))
-    const promiseArray = blogObjects.map(blog => blog.save())
-    await Promise.all(promiseArray)
+    await Blog.insertMany(initialBlogs)
 })
 
 test('blog object contains identification attribute called id', async () => {
@@ -26,25 +23,79 @@ test('blog object contains identification attribute called id', async () => {
     expect(res.body[0].id).toBeDefined()
 })
 
-test('add new blog', async () => {
-    const newBlog = {
-        title: 'Blog 3',
-        author: 'John Doe',
-        url: 'http://example.com',
-        likes: 20,
-    }
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
+describe('add new blog', () => {
+    test('with likes field given', async () => {
+        const newBlog = {
+            title: 'Blog 3',
+            author: 'John Doe',
+            url: 'http://example.com',
+            likes: 20,
+        }
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
 
-    const blogsAfter = await blogsInDb()
-    expect(blogsAfter).toHaveLength(initialBlogs.length + 1)
+        const blogsAfter = await blogsInDb()
+        expect(blogsAfter).toHaveLength(initialBlogs.length + 1)
 
-    const titles = blogsAfter.map(blog => blog.title)
-    expect(titles).toContain('Blog 3')
+        const titles = blogsAfter.map(blog => blog.title)
+        expect(titles).toContain('Blog 3')
+    })
+
+    test('with likes field not given', async () => {
+        const newBlog = {
+            title: 'Blog 3',
+            author: 'John Doe',
+            url: 'http://example.com',
+        }
+        const res = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        const blogsAfter = await blogsInDb()
+        expect(blogsAfter).toHaveLength(initialBlogs.length + 1)
+
+        const blog = blogsAfter.find(blog => blog.id === res.body.id)
+        expect(blog.likes).toEqual(0)
+    })
+
+    test('with title missing', async () => {
+        const newBlog = {
+            author: 'John Doe',
+            url: 'http://example.com',
+        }
+        const res = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+
+        const blogsAfter = await blogsInDb()
+        expect(blogsAfter).toHaveLength(initialBlogs.length)
+
+        expect(res.body.error.msg).toMatch('`title` is required')
+    })
+
+    test('with url missing', async () => {
+        const newBlog = {
+            title: 'Blog 3',
+            author: 'John Doe',
+        }
+        const res = await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+
+        const blogsAfter = await blogsInDb()
+        expect(blogsAfter).toHaveLength(initialBlogs.length)
+
+        expect(res.body.error.msg).toMatch('`url` is required')
+    })
 })
+
 
 afterAll(async () => {
     await mongoose.connection.close()
