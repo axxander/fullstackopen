701b83fbef1a71197d35c041f4ade3e83e0a4ecb
axxander <axander.gregory@gmail.com>
2021-11-13 22:01:44 +0000

    Changed to async-await syntax

--- part4/blog-backend/routes/blogs.routes.js
+++ part4/blog-backend/routes/blogs.routes.js
@@ -1,33 +1,33 @@
 const router = require('express').Router()
 
 const Blog = require('../models/blog.model')
-const { InternalError } = require('../utils/errors.utils')
+const { BadRequestError, InternalError } = require('../utils/errors.utils')
 
 
-router.get('', (req, res, next) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            return res.json(blogs)
-        })
-        .catch(() => {
-            // assume internal error
-            return next(new InternalError())
-        })
+router.get('', async (req, res, next) => {
+    try {
+        const blogs = await Blog.find({})
+        return res.json(blogs)
+    } catch {
+        // assume internal error
+        return next(new InternalError())
+    }
 })
 
-router.post('', (req, res, next) => {
-    const blog = new Blog(req.body)
+router.post('', async (req, res, next) => {
+    const body = req.body
+    if (!body.likes) {
+        body.likes = 0
+    }
 
-    blog
-        .save()
-        .then(savedBlog => {
-            return res.status(201).json(savedBlog)
-        })
-        .catch(() => {
-            // assume internal error
-            return next(new InternalError())
-        })
+    const blog = new Blog(body)
+    try {
+        const savedBlog = await blog.save()
+        return res.status(201).json(savedBlog)
+    } catch (e) {
+        // assume validation error
+        return next(new BadRequestError(e.message))
+    }
 })
 
 
