7b8e50b2c5aa1604bed32c7691af5cede143e03a
axxander <axander.gregory@gmail.com>
2021-11-14 17:06:20 +0000

    Added new routes

--- part4/blog-backend/routes/blogs.routes.js
+++ part4/blog-backend/routes/blogs.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 
 const Blog = require('../models/blog.model')
-const { BadRequestError, InternalError } = require('../utils/errors.utils')
+const { BadRequestError, InternalError, NotFoundError } = require('../utils/errors.utils')
 
 
 router.get('', async (req, res, next) => {
@@ -11,6 +11,16 @@ router.get('', async (req, res, next) => {
     } catch {
         // assume internal error
         return next(new InternalError())
+    }
+})
+
+router.get('/:id', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        const blog = await Blog.findById(id)
+        return blog ? res.json(blog) : next(new NotFoundError())
+    } catch {
+        return next(new BadRequestError('malformatted id'))
     }
 })
 
@@ -27,6 +37,35 @@ router.post('', async (req, res, next) => {
     } catch (e) {
         // assume validation error
         return next(new BadRequestError(e.message))
+    }
+})
+
+router.delete('/:id', async (req, res, next) => {
+    const id = req.params.id
+    try {
+        await Blog.findByIdAndDelete(id)
+        return res.sendStatus(204)
+    } catch {
+        // assume bad request not internal error for now
+        return next(new BadRequestError('malformatted id'))
+    }
+})
+
+router.put('/:id', async (req, res, next) => {
+    const body = req.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes,
+    }
+
+    const id = req.params.id
+    try {
+        const updatedBlog = await Blog.findByIdAndUpdate(id, blog, { new: true })
+        return updatedBlog ? res.json(updatedBlog) : next(new NotFoundError())
+    } catch {
+        return next(new BadRequestError('malformatted id'))
     }
 })
 
