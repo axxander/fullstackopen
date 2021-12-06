76d9e998edbc9fc430c0b15fcd40ddbd9f49a975
axxander <axander.gregory@gmail.com>
2021-12-06 23:01:19 +0000

    Added user and sessions routes

--- part3/notes-backend/routes/index.js
+++ part3/notes-backend/routes/index.js
@@ -1,10 +1,13 @@
 const router = require('express').Router()
 
 const notesRoutes = require('./notes.routes')
+const userRoutes = require('./users.routes')
+const sessionsRoutes = require('./sessions.routes')
 
 
-// Note routes
 router.use('/notes', notesRoutes)
+router.use('/users', userRoutes)
+router.use('/sessions', sessionsRoutes)
 
 
 module.exports = router
